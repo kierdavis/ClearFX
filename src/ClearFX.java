4c5e001a26cb4073406df366bb6056d29a3a784a
Kier Davis <kierdavis@gmail.con>
2013-05-09 16:58:28 +0100

    Initial code commit

--- src/ClearFX.java
+++ src/ClearFX.java
@@ -3,5 +3,7 @@ package com.kierdavis.clearfx;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ClearFX extends JavaPlugin {
-    
+    public void onEnable() {
+        getCommand("clearfx").setExecutor(new ClearFXCommandExecutor(this));
+    }
 }
