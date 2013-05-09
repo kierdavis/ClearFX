87edf9d9c267637cce8091c156250fc64d4382ed
Kier Davis <kierdavis@gmail.con>
2013-05-09 16:59:48 +0100

    Fixed compile errors

--- src/ClearFXCommandExecutor.java
+++ src/ClearFXCommandExecutor.java
@@ -1,10 +1,12 @@
 package com.kierdavis.clearfx;
 
+import java.util.Iterator;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
-import java.util.Iterator;
 
 public class ClearFXCommandExecutor implements CommandExecutor {
     private ClearFX plugin;
